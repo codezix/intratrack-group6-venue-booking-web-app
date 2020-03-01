caf59180dfecf84970b2360fa516145dc6630638
Ikem Nomso <ikemnomso@gmail.com>
2020-03-01 19:33:29 +0100

    modules

--- public/js/bookingFormHandler.js
+++ public/js/bookingFormHandler.js
@@ -1,21 +1,21 @@
 $(document).ready(() => {
-  const bookingForm = $(".bookingForm");
-  const bookingResponse = $(".bookingForm .bookingResponse");
+	const bookingForm = $(".bookingForm");
+	const bookingResponse = $(".bookingForm .bookingResponse");
 
-  bookingForm.submit(e => {
-    e.preventDefault();
-    const formData = new FormData(this);
-    fetch(bookingForm.action, { method: bookingForm.method, body: formData })
-      .then(res => {
-        res.json();
-      })
-      .then(data => {
-        console.log(data.bookingResponse);
-        // bookingResponse.text(data.bookingResponse);
-      })
-      .catch(err => {
-        console.log(err);
-        // bookingResponse.css(color, "red").text(err);
-      });
-  });
+	bookingForm.submit(e => {
+		e.preventDefault();
+		const formData = new FormData(this);
+		fetch(bookingForm.action, { method: bookingForm.method, body: formData })
+			.then(res => {
+				res.json();
+			})
+			.then(data => {
+				console.log(data.bookingResponse);
+				// bookingResponse.text(data.bookingResponse);
+			})
+			.catch(err => {
+				console.log(err);
+				// bookingResponse.css(color, "red").text(err);
+			});
+	});
 });
